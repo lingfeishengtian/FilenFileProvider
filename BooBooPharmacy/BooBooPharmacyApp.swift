26a0d9f9af4586126ac8b09075bf70fed5f7b883
lingfeishengtian <hunter.han@gmail.com>
2023-02-23 22:37:28 -0600

    Initial Commit

---
+++ BooBooPharmacy/BooBooPharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooBooPharmacyApp.swift
+//  BooBooPharmacy
+//
+//  Created by Hunter Han on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BooBooPharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
